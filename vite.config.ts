9eb9f189f063f02066cd26aeb8801b2d1665f916
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-09-19 16:59:30 +0200

    fix: resolve alias using url

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
-import viteReact from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import { resolve } from 'node:path';
+import viteReact from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [tanstackRouter({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
     resolve: {
         alias: {
-            '@': resolve(import.meta.url, './src')
+            '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     }
 });
